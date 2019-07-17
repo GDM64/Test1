7ea70b6c7ea409a85fbb02c936acfc738ee283b1
DysterBjorn <greg.linares@sfr.fr>
2019-07-17 21:58:38 +0200

    tri

--- npm-deckstrings-master/src/constants.ts
+++
@@ -1,8 +0,0 @@
-import { FormatType as FormatTypeType } from "../types";
-
-export const DECKSTRING_VERSION = 1;
-
-export const FormatType: { [key: string]: FormatTypeType } = {
-	FT_WILD: 1,
-	FT_STANDARD: 2,
-};
